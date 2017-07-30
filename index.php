1816735739a7e9e6c2138296fb5a4420f843e4a6
Jing523 <jing@britensw.com.au>
2017-07-30 15:56:12 +0800

    WIP

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Your Title</title>
+	<link rel="stylesheet" type="text/css" href=styles/1/basic.css />
+	<link rel="stylesheet" type="text/css" href=styles/1/index.css />
+</head>
+<body>
+
+<div id="header">
+	<h1><a href="index.php">飘城Web俱乐部多用户留言系统</a></h1>
+	<ul>
+		<li>首页</li>
+		<li>注册</li>
+		<li>登陆</li>
+		<li>个人中心</li>
+		<li>风格</li>
+		<li>管理</li>
+		<li>退出</li>
+	</ul>
+</div>
+
+<div id="list">
+	<h2>帖子列表</h2>
+</div>
+
+<div id="user">
+	<h2>新进会员</h2>
+</div>
+
+<div id="pics">
+	<h2>最新图片</h2>
+</div>
+
+<div id="footer">
+	<p>版权所有 翻版必究</p>
+	<p>本程序由<span>瓢城Web俱乐部</span>提供 源代码可以任意修改或发布（c）yc60.com</p>
+</div>
+
+</body>
+</html>
